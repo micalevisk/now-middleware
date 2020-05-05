55a12a08f744420c4b34861eff55e3ba883423e8
Maxence Dalmais <github@maxired.fr>
2020-05-05 16:54:23 +0200

    fix: Allow error middlewares (#26)
    * add error middleware
    
    * lint
    
    * used variable should not start with _

--- src/chain.spec.ts
+++ src/chain.spec.ts
@@ -46,4 +46,86 @@ describe('chain', () => {
     chain(dummyMiddleware)(dummyHandler)(req, res);
     expect(res.send).toHaveBeenCalledWith('bar');
   });
+
+  it('should work deal with error sended as param of next', () => {
+    // A dummy Express middleware
+    function errorMiddleware(
+      _req: Request,
+      _res: Response,
+      next: NextFunction
+    ): void {
+      next(new Error('errorMiddleware'));
+    }
+
+    function errorCatchMiddleware(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      err: any,
+      _req: Request,
+      res: Response,
+      next: NextFunction
+    ): void {
+      if (err) {
+        res.send(err.message);
+        return;
+      }
+      next();
+    }
+
+    // A dummy ZEIT now handler
+    function dummyHandler(req: NowRequest, res: NowResponse): void {
+      expect(req.headers.foo).toBe('foo');
+
+      res.send('bar');
+    }
+
+    const req = { headers: {} } as NowRequest;
+    const res = ({
+      send: jest.fn(),
+    } as unknown) as NowResponse;
+
+    chain(errorMiddleware, errorCatchMiddleware)(dummyHandler)(req, res);
+    expect(res.send).toHaveBeenCalledWith('errorMiddleware');
+    expect(res.send).not.toHaveBeenCalledWith('bar');
+  });
+
+  it('should work deal with error when thrown in the middleware', () => {
+    // A dummy Express middleware
+    function errorThrowMiddleware(
+      _req: Request,
+      _res: Response,
+      _next: NextFunction
+    ): void {
+      throw new Error('errorThrowMiddleware');
+    }
+
+    function errorCatchMiddleware(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      err: any,
+      _req: Request,
+      res: Response,
+      next: NextFunction
+    ): void {
+      if (err) {
+        res.send(err.message);
+        return;
+      }
+      next();
+    }
+
+    // A dummy ZEIT now handler
+    function dummyHandler(req: NowRequest, res: NowResponse): void {
+      expect(req.headers.foo).toBe('foo');
+
+      res.send('bar');
+    }
+
+    const req = { headers: {} } as NowRequest;
+    const res = ({
+      send: jest.fn(),
+    } as unknown) as NowResponse;
+
+    chain(errorThrowMiddleware, errorCatchMiddleware)(dummyHandler)(req, res);
+    expect(res.send).toHaveBeenCalledWith('errorThrowMiddleware');
+    expect(res.send).not.toHaveBeenCalledWith('bar');
+  });
 });
--- src/chain.ts
+++ src/chain.ts
@@ -15,7 +15,13 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import { NowRequest, NowResponse } from '@now/node';
-import { RequestHandler } from 'express';
+import {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from 'express';
 
 type AsyncVoid = void | Promise<void>;
 
@@ -24,26 +30,48 @@ type AsyncVoid = void | Promise<void>;
  */
 export type NowFunction<Req, Res> = (req: Req, res: Res) => AsyncVoid;
 
+function reduceMiddleware(
+  acc: RequestHandler,
+  mid: RequestHandler | ErrorRequestHandler
+): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction): void {
+    acc(req, res, (err) => {
+      if (err) {
+        if (mid.length === 4) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+          // @ts-ignore
+          return mid(err, req, res, next);
+        }
+        return next(err);
+      }
+      if (mid.length === 4) {
+        return next();
+      }
+      try {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        mid(req, res, next, undefined);
+      } catch (e) {
+        next(e);
+      }
+    });
+  };
+}
 /**
  * Combine multiple middleware together.
  *
- * @param middlewares - Functions of form: `function(req, res, next) { ... }`, aka
+ * @param middlewares - Functions of form: `function(req, res, next) { ... }` or `function(err, req, res, next) { ... }`, aka
  * express middlewares.
  *
  * @return - Single combined middleware
  */
-function combineMiddleware(middlewares: RequestHandler[]): RequestHandler {
-  return middlewares.reduce((acc, mid) => {
-    return function (req, res, next): void {
-      acc(req, res, (err) => {
-        if (err) {
-          return next(err);
-        }
-
-        mid(req, res, next);
-      });
-    };
-  });
+function combineMiddleware(
+  middlewares: (RequestHandler | ErrorRequestHandler)[]
+): RequestHandler | ErrorRequestHandler {
+  return middlewares.reduce(
+    reduceMiddleware,
+    (_req: Request, _res: Response, next: NextFunction) => next()
+  );
 }
 
 /**
@@ -54,7 +82,7 @@ function combineMiddleware(middlewares: RequestHandler[]): RequestHandler {
  * express middlewares.
  */
 export function chain<Req = NowRequest, Res = NowResponse>(
-  ...middlewares: RequestHandler[]
+  ...middlewares: (RequestHandler | ErrorRequestHandler)[]
 ): (fn: NowFunction<Req, Res>) => NowFunction<Req, Res> {
   return function (fn: NowFunction<Req, Res>): NowFunction<Req, Res> {
     return function (req: Req, res: Res): AsyncVoid {
